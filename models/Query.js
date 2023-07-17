39fc714738d7ac695021941ab7136d9f891a2f03
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-07-17 11:02:35 +0200

    Cambios de correcciones

--- models/Query.js
+++ models/Query.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const QuerySchema = new mongoose.Schema(
+const QuerySchema = new Schema(
     {
         topic: String,
         question: String,
         resolved: Boolean,
+        _idAnswer: {
+            type: Schema.Types.ObjectId,
+            ref: "Answer",
+        },
+
     },
     {
         timestamps: true,
