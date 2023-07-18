106655f69dce208539b5bb9765ffc4de50ffe725
JuandaMT <juandamt@outlook.es>
2023-07-18 11:44:06 +0200

    conflicto resuelto V2

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/UserController");
 const { authentication, isTeacher } = require("../middleware/authentication");
-const { authentication } = require("../middleware/authentication");
 
 router.post("/", UserController.create);
 router.post("/login", UserController.login);
